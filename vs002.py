b015bae8dad59880fcf748ec0dc2ebc467bcae67
NikKontos <ed19nk2@leeds.ac.uk>
2024-03-03 15:33:00 +0000

    functioning add,remove nodes on dbl click

--- vs002.py
+++ vs002.py
@@ -1,61 +1,104 @@
 import tkinter as tk
-from pyvis.network import Network
+
+import networkx as nx
 import webbrowser
 import os
 
+class Node:
+     def __init__(self, obj_id,node_id, x, y):
+        self.obj_id=obj_id
+        self.node_id = node_id
+        self.x = x
+        self.y = y
+
+class Edge:
+     def __init__(self, obj_id,node_s,node_t):
+        self.obj_id = obj_id
+        self.x1 = node_s.x
+        self.y1 = node_s.y
+        self.x2 = node_t.x
+        self.y2 = node_t.y
+        
+
+        
 class GraphGUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Graph GUI")
 
         # Create an empty graph
-        self.graph = Network(height="500px", width="100%", notebook=True, cdn_resources='in_line')
+        
 
         # Create a Tkinter canvas for pyvis
         self.canvas = tk.Canvas(self.root, width=800, height=600)
         self.canvas.pack()
 
         # Initialize variables to store nodes and edges
-        self.nodes = []
-        self.edges = []
-        self.draw = [[None, None]]
+        self.nodes=[]
+        self.edge=[]
+        
+        self.obj_id=0
 
         self.last_clicked_node = None
         self.current_edge = None  # Variable to track the current edge being drawn
 
         # Bind mouse events to functions
+
         self.canvas.bind("<Double-Button-1>", self.dbl_clk)
+    
+    def displayGraph(self):
+        self.canvas.delete("all")
+        for node in self.nodes:
+            r=30
+            print(node)
+            self.canvas.create_oval(node.x-r,node.y-r,node.x+r,node.y+r,fill="red")
+            self.canvas.create_text(node.x,node.y,text=str(node.node_id))
+            
+        self.canvas.pack()
+
 
     def proximal(self, event):
-        x = event.x
-        y = event.y
-        r = 60
+        
+        r = 30
 
-        for node_id, node_x, node_y in self.nodes:
-            distance = ((x - node_x) ** 2 + (y - node_y) ** 2) ** 0.5
+        for node in self.nodes:
+            print("node:",node.obj_id,node.x,node.y)
+            
+            distance = ((event.x - node.x) ** 2 + (event.y - node.y) ** 2) ** 0.5
             if distance <= r:
-                return node_id, node_x, node_y  # Double click within radius of an existing node
+                return node  # Double click within radius of an existing node
 
         return False  # Double click not within radius of any existing node
 
     def add_node(self, event):
         # Add a node at the clicked position
         x, y = event.x, event.y
-        num_node = len(self.nodes)
-        node_id = num_node + 1
-        self.nodes.append((node_id, x, y))
-
-        self.graph.add_node(node_id, x=x, y=y)  # Add the node to the graph at the clicked position
+        node_id = len(self.nodes)
+        n=Node(self.obj_id,node_id,x,y)
+        self.obj_id+=1
+        self.nodes.append(n)
 
         # Draw a circle representing the node on the Tkinter canvas
-        radius = 15
-        pict = self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill="blue",tag=str(node_id))
-        text = self.canvas.create_text(x, y, text=node_id,tag=str(node_id))
-        self.draw.append([pict, text])
+        radius = 30
+
+        
         # Save the last clicked node for adding edges
         self.last_clicked_node = node_id
+        self.displayGraph()
+        return self
 
-            return self
+    def remove_node(self,event,p):
+        
+        for node in self.nodes:
+            if p.x==node.x and p.y==node.y:
+                self.nodes.remove(node)
+
+
+        for edge in self.edge:
+            if (p.node_id==edge.node_s or p.node_id==edge.node_t):
+                self.edges.remove(edge)
+
+        self.displayGraph()
 
     def dbl_clk(self, event):
         p = self.proximal(event)
